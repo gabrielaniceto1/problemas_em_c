1b04a781551a539aab494c876c1e95f5e1947ed0
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2025-10-23 14:35:04 -0300

    adicionando funcoes de abb

--- Lista 10/arvore_binaria.c
+++ Lista 10/arvore_binaria.c
@@ -24,3 +24,25 @@ void inserir(Tree **head, int val){
         inserir(&(*head)->right,val);
     }
 }
+
+Tree* maior_valor(Tree *root) {
+    if (root == NULL) return NULL;
+    if (root->right == NULL) return root;
+    return maior_valor(root->right);
+}
+
+int altura_arvore(Tree *root){
+    if(root == NULL) return 0;
+    int hleft = altura_arvore(root->left);
+    int hright = altura_arvore(root->right);
+    return (hleft > hright ? hleft : hright) + 1;
+}
+
+void imprimir_folhas(Tree *root){
+    if(root == NULL) return;
+    if(root->left == NULL && root->right == NULL){
+        printf("%d\n", root->val);
+    }
+    imprimir_folhas(root->left);
+    imprimir_folhas(root->right);
+}
